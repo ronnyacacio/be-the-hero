7f780f2db9943cf1bc5ab327ee98151b33fd43b1
Ronny Ac[acio <ronnyacacio27@gmail.com>
2020-03-24 15:44:29 -0300

    backend complete

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
+
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ event: "Semana OmniStack 11.0" });
-});
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.destroy);
+
+routes.get("/profile", ProfileController.index);
+
+routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
